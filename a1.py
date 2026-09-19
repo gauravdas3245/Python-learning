bb9155b73502c3a9f18637abbaadb3ad3383f367
Gaurav <gauravdas3245@gmail.com>
2026-09-19 23:59:16 +0530

    python basics

---
+++ a1.py
@@ -0,0 +1,30 @@
+# Python Basics
+
+# 1. Print
+print("Hello Python")
+
+# 2. Variables
+name = "Gaurav"
+age = 18
+
+print(name)
+print(age)
+
+# 3. Input
+num = int(input("Enter a number: "))
+
+# 4. Even or Odd
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# 5. For Loop
+for i in range(1, 11):
+    print(i)
+
+# 6. While Loop
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
